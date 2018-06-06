8ed5d1796cd59f80350ea6d2f3844d923b1cc671
rajat0786 <rajat9779828014@gmail.com>
2018-06-06 21:09:01 +0530

    assingment 2

---
+++ assingment2.py
@@ -0,0 +1,31 @@
+Q1
+print("hello acadview")
+
+
+Q2
+print("acad"+"view")
+
+
+Q3
+a=1
+b=2
+c=3
+print(a,b,c)
+
+
+Q4
+print("let's get started")
+
+
+Q5
+s="acadview"
+course="python"
+fees=5000
+print("hi your are at %s your course is %s and it's fees is %d"%(s,course,fees))
+
+
+
+Q6
+name="tony stark"
+salary=1000000
+print('%s"%d'%(name,salary))
